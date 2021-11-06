94dbd04dc0b80de50dc402bfaaf2e0e581097be5
Alvinpt <90598958+Alvinpt@users.noreply.github.com>
2021-11-06 12:55:28 +0800

    Add files via upload

---
+++ luaspersegi.java
@@ -0,0 +1,21 @@
+mport java.util.Scanner;
+
+class luaspersegi {
+	public static void main(String args[]) {
+	Double  hasil, sisi;
+	
+	rumusLuasPersegi rumus = new rumusLuasPersegi();
+	
+	Scanner input = new Scanner(System.in);
+		
+	System.out.println("Menghitung Luas Persegi");
+	System.out.println();
+	System.out.print("Masukkan sisi(cm) : ");
+	sisi = input.nextDouble();
+	System.out.println();
+	
+	rumus.tampil();
+	rumus.hitung(sisi);
+
+}
+}
